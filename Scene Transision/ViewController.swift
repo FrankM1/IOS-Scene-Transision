47bafe1da60dbe23ae20696b94c2b2a87b1393a3
frank.gitonga <frank.gitonga@incentrocom>
2018-11-27 13:50:52 +0300

    Initial commit

---
+++ Scene Transision/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Scene Transision
+//
+//  Created by Frank on 11/12/18.
+//  Copyright © 2018 Qazana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "First VC"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
